3b585470fad34b9734bd92ca5bc7aebbce3d4909
Cristiano Morais <cristianopdmorais@gmail.com>
2024-06-22 14:41:03 -0300

    Publ1

--- main.py
+++ main.py
@@ -1,33 +1,34 @@
 sair = 1
 nota = 11
-prosseguir = 1
+titulo = "titulo"
 
-titulo = input('digite o nome do filme: ')
-while nota not in (0,1,2,3,4,5,6,7,8,9,10):
-    try:
-        while nota not in (0,1,2,3,4,5,6,7,8,9,10): 
+try:
+    titulo = input('digite o nome do filme: ')
+    while nota not in (0,1,2,3,4,5,6,7,8,9,10): 
+        try:
             nota = int(input('Digite a nota de 0 a 10: '))
             if nota not in (0,1,2,3,4,5,6,7,8,9,10):
-                print("Valores errados, vamos tentar novamente!")
-    except:
-        print("Valores errados")
+                print("Valores errados, vamos tentar novamente")
+        except:
+            print("Valores errados")
+except:
+    print("erro insperado")
 
-    ordem_filmes = {titulo: nota}
-    
+ordem_filmes = {titulo: nota}
 
 while sair != 0:
-    novo_titulo = input('digite o nome do filme: ')
-    nova_nota = int(input('Digite a nota de 0 a 10: '))
-    try:
-        while nova_nota not in (0,1,2,3,4,5,6,7,8,9,10): 
-            nova_nota = int(input('Digite a nota de 0 a 10: '))
-            if nova_nota not in (0,1,2,3,4,5,6,7,8,9,10):
-                print("Valores errados, vamos tentar novamente!")
-    except:
-        print("Valores errados")
-
-    ordem_filmes = {novo_titulo: nova_nota}
-    print("Deseja adicionar mais avaliações?")
+    titulo = input('digite o nome de outro filme: ')
+    nota = int(input('Digite a nota de 0 a 10: '))
+    while nota not in (0,1,2,3,4,5,6,7,8,9,10): 
+        try:
+            print("Valores errados, vamos tentar novamente")
+            nota = int(input('Digite a nota de 0 a 10: '))
+            if nota not in (0,1,2,3,4,5,6,7,8,9,10):
+                print("Valores errados, vamos tentar novamente")
+        except:
+            print("Valores errados")
+    ordem_filmes[titulo] = nota
+    
     try:
         sair = int(input("digite '0' para sair ou '1' inserir outro filme: "))
         print(ordem_filmes)
