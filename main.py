24fff75fbdf6a9bdd8ec97b0c2fe4e83d685db40
Cristiano Morais <cristianopdmorais@gmail.com>
2024-06-26 10:07:23 -0300

    002

--- main.py
+++ main.py
@@ -10,29 +10,40 @@ try:
             if nota not in (0,1,2,3,4,5,6,7,8,9,10):
                 print("Valores errados, vamos tentar novamente")
         except:
-            print("Valores errados")
+            print("Digite apenas números inteiros")
 except:
-    print("erro insperado")
+    print("erro inesperado")
 
 ordem_filmes = {titulo: nota}
 
 while sair != 0:
-    titulo = input('digite o nome de outro filme: ')
-    nota = int(input('Digite a nota de 0 a 10: '))
-    while nota not in (0,1,2,3,4,5,6,7,8,9,10): 
-        try:
-            print("Valores errados, vamos tentar novamente")
-            nota = int(input('Digite a nota de 0 a 10: '))
-            if nota not in (0,1,2,3,4,5,6,7,8,9,10):
-                print("Valores errados, vamos tentar novamente")
-        except:
-            print("Valores errados")
-    ordem_filmes[titulo] = nota
     
     try:
+        titulo = input('digite o nome de outro filme: ')
+        nota = 11
+        while nota not in (0,1,2,3,4,5,6,7,8,9,10): 
+            try:
+                nota = int(input('Digite a nota de 0 a 10: '))
+                if nota not in (0,1,2,3,4,5,6,7,8,9,10):
+                    print("Valores errados, vamos tentar novamente")
+
+            except:
+                print("Digite apenas números inteiros")
+        ordem_filmes[titulo] = nota
+
+    except:
+        print("erro insperado")
+        
+    try:
         sair = int(input("digite '0' para sair ou '1' inserir outro filme: "))
-        print(ordem_filmes)
+        if sair == 1:
+            print("Ok")
+        elif sair == 0:
+            print("Segue abaixo os dados solicitados")
+
     except:
         print("Vou entender que queria digitar '1'")
+
+print(ordem_filmes)
 
 print([max(ordem_filmes, key=ordem_filmes.get)])
